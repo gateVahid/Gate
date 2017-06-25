1bea41b6dbf3922ab4f5f31ffe9885a1466070a3
gateVahid <vahid_fattahzadeh@yahoo.com>
2017-06-25 14:34:17 +0430

    expander added

---
+++ library/src/main/java/org/gateco/library/Expander.java
@@ -0,0 +1,40 @@
+package org.gateco.library;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Vahid on 06/25/2017.
+ */
+
+public class Expander extends LinearLayout {
+
+    LinearLayout container;
+    Button info;
+
+    public Expander(Context c) {
+        super(c);
+        LayoutInflater.from(c).inflate(R.layout.expander, this);
+        setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
+        container = (LinearLayout) findViewById(R.id.expander_container_shortinfo);
+        info = (Button) findViewById(R.id.expander_button_info);
+
+
+    }
+
+    public LinearLayout getContainer() {
+        return this.container;
+    }
+
+    public Button getInfo() {
+        return this.info;
+    }
+
+
+}
+
