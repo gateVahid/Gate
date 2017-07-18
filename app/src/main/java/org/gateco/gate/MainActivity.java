c94786973bb2e88068fdfb566bcfcf9bafa93f25
gateVahid <vahid_fattahzadeh@yahoo.com>
2017-07-18 21:39:27 +0430

    adassd

--- app/src/main/java/org/gateco/gate/MainActivity.java
+++ app/src/main/java/org/gateco/gate/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dialog_make_sure);
+        setContentView(R.layout.cardview_small);
 
 
 
