486f9c672c999f9ae04efc7f2254d0a1c3698cc6
gateVahid <vahid_fattahzadeh@yahoo.com>
2017-07-19 21:54:46 +0430

    yess

---
+++ library/src/main/java/org/gateco/library/DetailViewEditable.java
@@ -0,0 +1,52 @@
+package org.gateco.library;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Vahid on 06/25/2017.
+ */
+
+public class DetailViewEditable extends LinearLayout {
+
+    TextView name, value;
+    ImageView close;
+
+    public DetailViewEditable(Context c) {
+        super(c);
+        LayoutInflater.from(c).inflate(R.layout.detailview,this);
+        setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        value = (TextView) findViewById(R.id.dveditable_textview_value);
+        name = (TextView) findViewById(R.id.dveditable_textview_name);
+        close = (ImageView)findViewById(R.id.dveditable_imageview_close);
+
+
+    }
+
+    public TextView getname() {
+        return this.name;
+    }
+
+    public TextView getvalue() {
+        return this.value;
+    }
+
+    public ImageView getClose(){
+        return this.close;
+    }
+
+
+    public void setTypeFace(Typeface tf){
+
+        name.setTypeface(tf);
+        value.setTypeface(tf);
+    }
+
+}
---
+++ library/src/main/java/org/gateco/library/DialogDetail.java
@@ -0,0 +1,70 @@
+package org.gateco.library;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+/**
+ * Created by Vahid on 06/27/2017.
+ */
+
+public class DialogDetail extends Dialog {
+
+    TextView fieldTV,valueTV;
+    EditText fieldET,valueET;
+    Button ok;
+
+    public DialogDetail(Context c){
+        super(c);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialoge_passchange);
+        getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        setCanceledOnTouchOutside(true);
+        setCancelable(true);
+
+        fieldTV=(TextView)findViewById(R.id.dd_textview_fieldTV);
+        valueTV=(TextView)findViewById(R.id.dd_textview_valueTV);
+        fieldET=(EditText)findViewById(R.id.dd_edittext_fieldET);
+        valueET=(EditText)findViewById(R.id.dd_edittext_valueET);
+        ok=(Button)findViewById(R.id.dd_button_ok);
+
+    }
+
+    public TextView getFieldTV(){
+        return this.fieldTV;
+    }
+    public TextView getValueTV(){
+        return this.valueTV;
+    }
+    public EditText getFieldET(){
+        return this.fieldET;
+    }
+    public EditText getValueET(){
+        return this.valueET;
+    }
+    public  Button getOk(){
+        return this.ok;
+    }
+
+
+    public void setTypeFace(Typeface tf){
+
+        fieldTV.setTypeface(tf);
+        valueTV.setTypeface(tf);
+        fieldET.setTypeface(tf);
+        valueET.setTypeface(tf);
+
+        ok.setTypeface(tf);
+    }
+
+
+}
